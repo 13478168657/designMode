b39c9ab28b50c0e3565a391cab519e81dead6e0f
ykk <ykk@qq.com>
2020-11-20 16:15:39 +0800

    commit

--- lara/container/Ioc.php
+++ lara/container/Ioc.php
@@ -5,7 +5,34 @@ namespace container;
 
 class Ioc{
 
-    public $binding = [];
+
+    public function make($obj){
+
+        $reflection = new \ReflectionClass($obj);
+
+        $constructor = $reflection->getConstructor();
+
+        if(is_null($constructor)){
+
+            return $reflection->newInstance();
+        }
+        $parameters = $constructor->getParameters();
+        $dependences = $this->getDependency($parameters);
+
+        return $reflection->newInstanceArgs($dependences);
+        
+    }
 
 
+    protected function getDependency($parameters){
+
+        $dependences = [];
+
+        foreach($parameters as $param){
+
+            $dependences[] = $this->make($param);
+        }
+        return $dependences;
+    }
 }
+
